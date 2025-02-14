44f4591a30112dc5e9d4b0821b05721d709bcc85
Dominik Selzer <dominik.selzer@uni-saarland.de>
2025-02-14 10:50:25 +0100

    Add test for fetch etas that fails

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -31,3 +31,15 @@ test_that("Read Etas work", {
   expect_equal(result[1, 3], 1.945E1)
   expect_equal(result[1, 4], 5.0757)
 })
+
+test_that("Read Etas work with no covar", {
+  file <- read_lst_file("theta_no_se.lst")
+  result <- fetch_etas(file)
+
+  expect_equal(nrow(result), 7)
+  expect_equal(ncol(result), 4)
+  expect_equal(result[1, 1], "ETA1")
+  expect_equal(result[1, 2], 0.2)
+  expect_equal(result[1, 3], NA_real_)
+  expect_equal(result[1, 4], NA_real_)
+})
